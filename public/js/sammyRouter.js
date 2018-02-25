f5457069414e6790183965f0aff8443d15f92b98
mkovachev <martin.kovachev@outlook.com>
2018-02-25 16:22:46 +0200

    .

--- public/js/sammyRouter.js
+++ public/js/sammyRouter.js
@@ -1,12 +1,9 @@
 const sammyApp = Sammy('#app-Container', function () {
     let $appContainer = $('#app-Container');
 
-<<<<<<< HEAD
+
     this.get('#/login', context => {
         db.user.login()
-=======
-    this.get('#/login', function (context) {
-        db.login()
             .then(res => {
                 events = res.result;
                 return handlebarsCompiler.compile('login');
@@ -38,40 +35,6 @@ const sammyApp = Sammy('#app-Container', function () {
             });
     });
 
-
-    this.get('#/login', () => {
-        db.getAll()
->>>>>>> remotes/origin/master
-            .then(res => {
-                events = res.result;
-                return handlebarsCompiler.compile('login');
-            })
-            .then(template => {
-                $appContainer.html(template(events));
-
-                // put login
-                $('#btn-login').on('click', () => {
-                    const user = {
-                        email: $('#email').val(),
-                        passHash: $('#password').val()
-                    };
-
-                    data.users.login(user)
-                        .then((user) => {
-                            context.redirect('#/home');
-                        })
-                });
-
-                // post register
-                $('#btn-register').on('click', () => {
-                    const user = {
-                        email: $('#email').val(),
-                        passHash: $('#password').val()
-                    };
-
-                });
-            });
-    });
 
     this.get('#/home', () => {
         let events = null;
