bb1bcfa0b77b1474fcebae7f97af9c1c4797c6b9
mkovachev <martin.kovachev@outlook.com>
2018-02-25 15:55:26 +0200

    .

--- public/js/sammyRouter.js
+++ public/js/sammyRouter.js
@@ -1,6 +1,40 @@
 const sammyApp = Sammy('#app-Container', function () {
     let $appContainer = $('#app-Container');
 
+    this.get('#/login', function (context) {
+        db.login()
+            .then(res => {
+                events = res.result;
+                return handlebarsCompiler.compile('login');
+            })
+            .then(template => {
+                $appContainer.html(template(events));
+
+                // put login
+                $('#btn-login').on('click', () => {
+                    const user = {
+                        email: $('#email').val(),
+                        passHash: $('#password').val()
+                    };
+
+                    data.users.login(user)
+                        .then((user) => {
+                            context.redirect('#/home');
+                        })
+                });
+
+                // post register
+                $('#btn-register').on('click', () => {
+                    const user = {
+                        email: $('#email').val(),
+                        passHash: $('#password').val()
+                    };
+
+                });
+            });
+    });
+
+
     this.get('#/login', () => {
         db.getAll()
             .then(res => {
